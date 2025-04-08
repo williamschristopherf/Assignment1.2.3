763294a772955b2c1a6719b45b0423583f966e4c
Christopher Williams <williamstophef@gmail.com>
2025-04-08 14:33:49 -0700

    Added a function for fun to hold the switch ctatement for multpile uses if necessary.

--- Program.cs
+++ Program.cs
@@ -9,17 +9,10 @@ double num2 = 0;
 bool exit = false;
 char mathOperator = 'a';
 char toContinue = 'y';
-//While loop to keep the program going 
-while (exit == false)
-{
-    //Get user input for 2 numbers and the mathmatical operator
-    Console.WriteLine("Enter your first number: ");
-    num1 = Convert.ToDouble(Console.ReadLine());
-    Console.WriteLine("Enter your second value: ");
-    num2 = Convert.ToDouble(Console.ReadLine());
-    Console.WriteLine("Do you want to add, subtract, mutiply, or divide? \"a, s, m, d\"");
-    mathOperator = Convert.ToChar(Console.ReadLine());
 
+
+static void Calculator(double num1, double num2, double answer, char mathOperator)
+{
     //Switch statements to handle the different math operations. Default to catch inavlid charachter inputs
     switch (mathOperator)
     {
@@ -47,10 +40,26 @@ while (exit == false)
             Console.WriteLine("Invalid Entry");
             break;
     }
+}
+
+
+//While loop to keep the program going 
+while (exit == false)
+{
+    //Get user input for 2 numbers and the mathmatical operator
+    Console.WriteLine("Enter your first number: ");
+    num1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter your second value: ");
+    num2 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Do you want to add, subtract, mutiply, or divide? \"a, s, m, d\"");
+    mathOperator = Convert.ToChar(Console.ReadLine());
+
+    //Calling the function that does the math for the calculator
+    Calculator(num1, num2, answer, mathOperator);
 
     //User input to continue or break the while loop based on comparing toContinue char in the if statement
     Console.WriteLine("Do you have another input? y or n");
-    toContinue = Convert.ToChar(Console.ReadLine());
+    toContinue = Convert.ToChar(Console.ReadLine().ToLower());
     if (toContinue == 'n')
     {
         exit = true;
